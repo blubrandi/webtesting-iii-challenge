908cf044ca20c382a7d8f14fe83cb2cefbedbaaa
blubrandi <blubrandi0@gmail.com>
2019-05-15 16:48:22 -0600

    Added run test in Dashboard.spec.js

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,3 +1,11 @@
 // Test away
 
 import React from 'react'
+import Dashboard from './Dashboard.js'
+
+describe('<Dashboard />', () => {
+
+    it('runs the tests', () => {
+        expect(true).toBe(true)
+    })
+})
