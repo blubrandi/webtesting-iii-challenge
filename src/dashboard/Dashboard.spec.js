d00612d672c22e6d0098f4898e9e2625574f4e0c
blubrandi <blubrandi0@gmail.com>
2019-05-16 10:22:41 -0600

    Added tests for Dashboard - run test, button test

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,11 +1,40 @@
-// Test away
-
 import React from 'react'
+import ReactDom from 'react-dom'
+
 import Dashboard from './Dashboard.js'
+
+import { render, fireEvent } from 'react-testing-library'
+import { toBeEnabled, toBeDisabled } from "jest-dom/extend-expect"
+import 'react-testing-library'
 
 describe('<Dashboard />', () => {
 
     it('runs the tests', () => {
         expect(true).toBe(true)
+    })
+
+    // it('should be unlocked by default and change to locked when clicked', () => {
+    //     const { getByText } = render(<Dashboard />)
+
+    //     const button = getByText(/unlocked/i)
+    //     fireEvent.click(button)
+
+    //     getByText(/locked/i)
+    // })
+
+    it('should be unlocked by default and change to locked when clicked', () => {
+        const { toBeDisabled } = render(<Dashboard />)
+
+        const button = getByText(/unlocked/i)
+        fireEvent.click(button)
+
+        getByText(/locked/i)
+    })
+
+    it('should be unlocked by default and change to locked when clicked', () => {
+        const { getByText } = render(<Dashboard />)
+
+        const button = getByText(/locked/i)
+
     })
 })
